869a65049e5ee3b0fa758fe9b2f8a555d7c8dbb3
onegoodyear <wesbedouinson@gmail.com>
2025-11-22 08:48:58 +0300

    Grades, Subjects Reading, Grades writting

--- main.go
+++ main.go
@@ -24,18 +24,22 @@ type Subject struct {
 }
 
 type Grade struct {
-	grade_id int
+	id int
 	student_id int
 	subject_id int
 	mark float32
 }
 
-
+func displayFiles (files []string) {
+	for i, file := range files {
+		fmt.Printf("\n %d. %s", i+1, file)
+	}
+}
 
 func main() {
-	// var grades []Grade = []Grade{}
+	var grades []Grade = []Grade{}
 	var students []Student = []Student{}
-	// var subjects []Subject = []Subject{}
+	var subjects []Subject = []Subject{}
 	entries, err  := os.ReadDir(".")
 	if err != nil {
 		fmt.Println("Something bad is happening with your computer!")
@@ -51,12 +55,17 @@ func main() {
 	}
 	fmt.Println("----------Welcome to Students Manager-----------")
 	fmt.Println("First you'll choose the file of the list of students:")
-	for i, file := range xlsxFiles {
-		fmt.Printf("\n%d. %s\n", i+1, file)
-	}
+	displayFiles(xlsxFiles)
 	var filenumber int
-	fmt.Print("\nPlease enter the number of the right file: ")
-	fmt.Scanf("%d", &filenumber)
+	for {
+		fmt.Print("\nPlease enter the number of the right file: ")
+		_, err = fmt.Scanln(&filenumber)
+		if err == nil && filenumber > 0 && filenumber <= len(xlsxFiles) {
+			break
+		}
+		fmt.Println("Invalid Input. Try Again!!")
+	}
+
 
 	// Reading the List of students
 
@@ -79,5 +88,102 @@ func main() {
 			id: idInt,
 		})
 	}
-	fmt.Println(students)
+
+	studentsFile.Close()
+
+	// Reading the list of subjects
+
+	fmt.Println("Second you'll choose the file of the list of subjects:")
+	displayFiles(xlsxFiles)
+	for {
+		fmt.Print("\nPlease enter the number of the right file: ")
+		_, err = fmt.Scanln(&filenumber)
+		if err == nil && filenumber > 0 && filenumber <= len(xlsxFiles) {
+			break
+		}
+		fmt.Println("Invalid Input. Try Again!!")
+	}
+
+	subjectsFile, err := excelize.OpenFile(xlsxFiles[filenumber-1])
+	if err != nil {
+		fmt.Println("There was a problem opening the students file, ", err)
+	}
+
+	rows, err = subjectsFile.GetRows("Sheet1")
+	if err != nil {
+		fmt.Println("There was a problem opening the subjects file, ", err)
+	}
+	for i := 1; i<len(rows); i++ {
+		row := rows[i]
+		idInt, _ := strconv.Atoi(row[3])
+		subjects = append(subjects, Subject{
+			id: idInt,
+			title: row[1],
+			description: row[2],
+			teacher: row[0],
+		})
+	}
+
+	// Reading the list of the grades
+
+	fmt.Println("Now it's the time to select the grades file: ")
+	displayFiles(xlsxFiles)
+	for {
+		fmt.Println("\nPlease enter the right number: ")
+		_, err := fmt.Scanln(&filenumber)
+		if err == nil && filenumber > 0 && filenumber <= len(xlsxFiles) {
+			break
+		}
+		fmt.Println("Invalid Input, Try Again.")
+	}
+	
+	gradesFile, err := excelize.OpenFile(xlsxFiles[filenumber-1])
+	if err != nil {
+		fmt.Println("There was a problem opening the grades file, ", err)
+	}
+	rows, err = gradesFile.GetRows("Sheet1")
+	if err != nil {
+		fmt.Println("There were a problem getting the rows of the grades file")
+	}
+	for i := 1; i < len(rows); i++ {
+		row := rows[i]
+		idInt, _ := strconv.Atoi(row[0])
+		studentId, _ := strconv.Atoi(row[1])
+		subjectId, _ := strconv.Atoi(row[2])
+		mark64, _ := strconv.ParseFloat(row[3], 32)
+		mark := float32(mark64)
+		grades = append(grades, Grade {
+			id: idInt,
+			student_id: studentId,
+			subject_id: subjectId,
+			mark: mark,
+		})
+	}
+
+	// Writting the results in output.txt
+
+	outputFile, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println("An error occured when creating the output file, ", err)
+	}
+	outputFile.WriteString("--------The List of Grades (written)-------\n")
+
+	for i, grade := range grades {
+		var studentName string
+		for _, student := range students {
+			if student.id == grade.student_id {
+				studentName = student.name
+				break
+			}
+		}
+		var subjectTitle string
+		for _, subject := range subjects {
+			if (subject.id == grade.subject_id) {
+				subjectTitle = subject.title
+				break
+			}
+		}
+		outputFile.WriteString(fmt.Sprintf("%d. %s took %f on: %s\n", i+1, studentName, grade.mark, subjectTitle))
+	}
+
 }
